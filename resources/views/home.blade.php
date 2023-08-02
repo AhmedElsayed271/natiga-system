1b40c6cf49b490d0b0e30b2d1e9aa1a4047801fd
mahmoud <mahmoud123@gmail.com>
2023-08-02 06:49:18 +0300

    mahmoud

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,8 @@
             <img src="{{ asset('assets/frontend/images/logo.png') }}" alt="logo" width="80" />
           </div>
           <div class="text mx-2">
-            <h1 class="fw-bolder">الاداره الذاتيه الديمقراطيه</h1>
+            <h1 class="fw-bolder h3"> الموقع الرسمي للإدارة الذاتية لشمال وشرق سوريا
+هيئة التربية والتعليم في إقليم الجزيرة</h1>
             <span class="fs-3">هيئه التربية والتعليم</span>
           </div>
         </div>
@@ -31,7 +32,7 @@
         <div class="content text-center my-5">
           <div class="text-content">
             <h1 class="fw-bolder">
-              التعليم الاساسي <span class="sp-color">2023-2024</span>
+              التعليم الاساسي والثانوي<span class="sp-color">2023-2024</span>
             </h1>
           </div>
           @if(Session::get('error'))
@@ -50,32 +51,32 @@
             <div class="alert-danger alert">{{ $message }}</div>
           </div>
         @endif
-  
+
           <div class="inp">
             <div class="mb-3 text-end">
               <form action="{{ route('natiga.thanway') }}" method="post" id="natigaForm">
                 @csrf
                               <label for="exampleFormControlInput1" class="form-label fs-3"
-                >رقم البطاقه</label
+                >رقم الإكتتاب </label
               >
               <input
                 type="text"
                 class="form-control fs-3"
                 id="exampleFormControlInput1"
-                placeholder="ادخل رقم البطاقه"
+                placeholder="ادخل رقم الإكتتاب "
                 name="code"
               />
               <select name="section"
               class="form-select form-select-lg mb-3 my-3 fs-4 text-muted"
               aria-label="Large select example"
-              required 
+              required
             >
             <option selected disabled >اختر الفرع </option>
-              <option value="1"> الثانوية (علمي و ادبي)</option>
+              <option value="1">الاحرار عربي(اعدادي و ثانوي)</option>
               <option value="2">الاعدادي</option>
-              <option value="3">الاحرار عربي</option>
+              <option value="3"> الثانوي(علمي وادبي)</option>
               <option value="4">الاحرار كردي</option>
-              <option value="5">ثانوي مهني</option>
+              <option value="5"> (عربي و كردي)ثانوي مهني</option>
             </select>
               </form>
             </div>
@@ -85,7 +86,7 @@
         </div>
       </main>
       <footer class="fixed-bottom text-center py-1 w-100">
-        <p class="text-black fw-bold"> جميع الحقوق الإدارة  الذاتيه محفوظة لشمال شرق سوريا هيئه التربيه والتعليم 2023</p>
+        <p class="text-black fw-bold"> جميع الحقوق محفوظة للإدارة الذاتية لشمال وشرق سوريا _ هيئة التربية والتعليم في إقليم الجزيرة 2023</p>
       </footer>
     </div>
   </body>
